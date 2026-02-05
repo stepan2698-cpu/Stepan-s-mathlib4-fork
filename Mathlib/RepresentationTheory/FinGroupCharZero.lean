99df5801e506d1e84b6dac7df0770de14abc9f72
Stepan <stepurik@stanford.edu>
2026-02-04 16:17:30 -0800

    fix

--- Mathlib/RepresentationTheory/FinGroupCharZero.lean
+++ Mathlib/RepresentationTheory/FinGroupCharZero.lean
@@ -116,19 +116,33 @@ If `G` is finite and `k` an algebraically closed field of characteristic `0`,
 then an object of `FDRep k G` is simple if and only if its character has norm `1`.
 -/
 lemma simple_iff_char_is_norm_one [CharZero k] (V : FDRep k G) :
+    have : Fintype G := Fintype.ofFinite G
     Simple V ↔ ∑ g : G, V.character g * V.character g⁻¹ = Nat.card G where
   mp h := by
+    let : Fintype G := Fintype.ofFinite G
+    have : NeZero (Nat.card G : k) := by
+      rw [← @Fintype.card_eq_nat_card G this]
+      exact NeZero.charZero
     have := invertibleOfNonzero (NeZero.ne (Nat.card G : k))
+    have := invertibleOfNonzero (NeZero.ne (Fintype.card G : k))
     classical
     have : ⅟(Nat.card G : k) • ∑ g, V.character g * V.character g⁻¹ = 1 := by
-      simpa only [Nonempty.intro (Iso.refl V), ↓reduceIte] using char_orthonormal V V
+      simpa only [Nonempty.intro (Iso.refl V), ↓reduceIte, Fintype.card_eq_nat_card]
+      using char_orthonormal V V
     apply_fun (· * (Fintype.card G : k)) at this
-    rwa [mul_comm, ← smul_eq_mul, smul_smul, mul_invOf_self, smul_eq_mul, one_mul, one_mul] at this
+    rwa [mul_comm, ← smul_eq_mul, smul_smul, Fintype.card_eq_nat_card, mul_invOf_self, smul_eq_mul,
+    one_mul, one_mul] at this
   mpr h := by
+    let : Fintype G := Fintype.ofFinite G
+    have : NeZero (Nat.card G : k) := by
+      rw [← @ Fintype.card_eq_nat_card G this]
+      exact NeZero.charZero
+    have := invertibleOfNonzero (NeZero.ne (Fintype.card G : k))
     have := invertibleOfNonzero (NeZero.ne (Nat.card G : k))
     have eq := FDRep.scalar_product_char_eq_finrank_equivariant V V
     rw [h] at eq
-    simp only [invOf_eq_inv, smul_eq_mul, inv_mul_cancel_of_invertible] at eq
+    simp only [invOf_eq_inv, smul_eq_mul, inv_mul_cancel_of_invertible, Fintype.card_eq_nat_card]
+    at eq
     rw [simple_iff_end_is_rank_one, ← Nat.cast_inj (R := k), ← eq, Nat.cast_one]
 
 end FDRep
